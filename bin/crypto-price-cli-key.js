a6c4394bd430c4e9e5b244b3530149efbb03b050
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-08-29 16:45:33 +0600

    refactor: show, remove key added and key function created

--- bin/crypto-price-cli-key.js
+++ bin/crypto-price-cli-key.js
@@ -1,9 +1,20 @@
 const program = require('commander');
+const key = require('../commands/key');
 
 
 program
     .command('set')
     .description('Set API Key -- https://nomics.com')
-    .action(() => {console.log('Hello From Set')})
+    .action(key.set)
+
+program
+    .command('show')
+    .description('Show API Key')
+    .action(key.show)
+
+program
+    .command('remove')
+    .description('Remove API Key')
+    .action(key.remove)
 
 program.parse(process.argv);
